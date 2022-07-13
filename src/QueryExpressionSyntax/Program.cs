73afa0d07f97d9eba8a8abe91fb90ff6d498bbc7
Gustavo Luiz <70780066+luiz-lgrp@users.noreply.github.com>
2022-07-13 19:57:33 -0300

    updat list

--- src/QueryExpressionSyntax/Program.cs
+++ src/QueryExpressionSyntax/Program.cs
@@ -35,64 +35,159 @@ var listaCategorias = new List<Categoria>()
 
 
 
-// 01 - FILTRAR POR NOME
-//var resultado = from produto in listaProdutos
-//                where produto.Nome.ToLower() == "Arroz".ToLower()
-//                select produto;
+Console.WriteLine("01 - FILTRAR POR NOME");
+Console.WriteLine("");
+var resultado = from produto in listaProdutos
+                where produto.Nome.ToLower() == "Arroz".ToLower()
+                select produto;
 
-//foreach (var result in resultado)
-//{
-//    Console.WriteLine($"Nome: {result.Nome} | Valor: {result.Valor}");
-//}
+foreach (var result in resultado)
+{
+    Console.WriteLine($"Nome: {result.Nome} | Valor: {result.Valor}");
+}
 
-
-// 02 - FILTRAR PELO INICIO DA LETRA
-//var resultado = from produto in listaProdutos
-//                where produto.Nome.ToLower().Substring(0,1) == "M".ToLower()
-//                select produto;
-
-//foreach (var result in resultado)
-//{
-//    Console.WriteLine($"Nome: {result.Nome} | Valor: {result.Valor}");
-//}
+Console.WriteLine("");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+Console.WriteLine("");
 
 
-// 03 - FILTRAR POR PRIMEIRA LETRA E STATUS
-//var resultado = from produto in listaProdutos
-//                where produto.Nome.ToLower().Substring(0,1) == "M".ToLower() && produto.Status != true
-//                select produto;
+Console.WriteLine("02 - FILTRAR PELO INICIO DA LETRA");
+Console.WriteLine("");
+var resultado2 = from produto in listaProdutos
+                where produto.Nome.ToLower().Substring(0, 1) == "M".ToLower()
+                select produto;
 
-//foreach (var result in resultado)
-//{
-//    Console.WriteLine($"Nome: {result.Nome} | Status: {result.Status}");
-//}
+foreach (var result in resultado2)
+{
+    Console.WriteLine($"Nome: {result.Nome} | Valor: {result.Valor}");
+}
 
-
-// 04 - ORDENAR POR ID DECRESCENTE
-//var resultado = from produto in listaProdutos
-//                orderby produto.Id descending 
-//                select produto;
-
-//foreach (var result in resultado)
-//{
-//    Console.WriteLine($"Id: {result.Id} | Nome: {result.Nome} | Valor: {result.Valor}");
-//}
+Console.WriteLine("");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+Console.WriteLine("");
 
 
+Console.WriteLine("03 - FILTRAR PELA PRIMEIRA LETRA E STATUS");
+Console.WriteLine(""); 
+var resultado3 = from produto in listaProdutos
+                where produto.Nome.ToLower().Substring(0, 1) == "M".ToLower() && produto.Status != true
+                select produto;
 
-// 05 - SOMENTE PRODUTOS ENTRE DETERMINADOS VALORES
-//var resultado = from produto in listaProdutos
-//                where produto.Valor > 500 && produto.Valor < 4000
-//                select produto;
+foreach (var result in resultado3)
+{
+    Console.WriteLine($"Nome: {result.Nome} | Status: {result.Status}");
+}
 
-//foreach (var result in resultado)
-//{
-//    Console.WriteLine($" Nome: {result.Nome} | Valor: {result.Valor}");
-//}
-
+Console.WriteLine("");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+Console.WriteLine("");
 
 
-// 06 - AULA 4
+Console.WriteLine("04 - ORDENAR POR ID DECRESCENTE");
+Console.WriteLine(""); 
+var resultado4 = from produto in listaProdutos
+                orderby produto.Id descending
+                select produto;
+
+foreach (var result in resultado4)
+{
+    Console.WriteLine($"Id: {result.Id} | Nome: {result.Nome} | Valor: {result.Valor}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+Console.WriteLine("");
+
+
+Console.WriteLine("05 - SOMENTE PRODUTOS ENTRE DETERMINADOS VALORES");
+Console.WriteLine(""); 
+var resultado5 = from produto in listaProdutos
+                where produto.Valor > 500 && produto.Valor < 4000
+                select produto;
+
+foreach (var result in resultado5)
+{
+    Console.WriteLine($" Nome: {result.Nome} | Valor: {result.Valor}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+Console.WriteLine("");
+
+
+Console.WriteLine("06 - RETORNANDO NOME E VALOR USANDO OBJETO ANONIMO");
+Console.WriteLine(""); 
+var resultado6 = from produto in listaProdutos
+                select new { produto.Nome, produto.Valor };
+
+foreach (var result in resultado6)
+{
+    Console.WriteLine($" Nome: {result.Nome} | Valor: {result.Valor}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+Console.WriteLine("");
+
+
+Console.WriteLine("07 - RETORNANDO NOME E VALOR USANDO OUTRA CLASSE");
+Console.WriteLine(""); 
+//DESSE JEITO POSSO FAZER OPERAÇÕES MATEMATICAS
+var resultado7 = from produto in listaProdutos
+                select new ProdutoDto
+                {
+                    Nome = produto.Nome,
+                    Valor = produto.Valor + 20
+                };
+
+foreach (var result in resultado7)
+{
+    Console.WriteLine($" Nome: {result.Nome} | Valor: {result.Valor}");
+}
+
+Console.WriteLine("");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+Console.WriteLine("");
+
+
+Console.WriteLine("08 - AGRUPAMENTOS POR CATEGORIA");
+Console.WriteLine("");
+var resultado8 = from produto in listaProdutos
+                group produto by produto.CategoriaId into prodAgrupados
+                select prodAgrupados;
+
+foreach (var result in resultado8)
+{
+    Console.WriteLine("");
+    Console.WriteLine($"Grupo: {result.Key}");
+    Console.WriteLine("");
+    foreach (var produto in result)
+    {
+        Console.WriteLine($" Produto: {produto.Nome} | Categoria: {produto.CategoriaId}");
+    }
+
+}
+
+Console.WriteLine("");
+Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+Console.WriteLine("");
+
+Console.WriteLine("09 - RETORNANDO NOME DO PRODUTO E NOME DA CATEGORIA");
+Console.WriteLine("");
+var resultado9 = from produto in listaProdutos
+                 join cat in listaCategorias
+                 on produto.CategoriaId equals cat.Id
+                 select new
+                 {
+                     Produto = produto,
+                     Categoria = cat
+                 };
+
+    foreach (var produto in resultado9)
+    {
+        Console.WriteLine($" Produto: {produto.Produto.Nome} | Categoria: {produto.Categoria.Nome}");
+    };
+
 
 
 
@@ -111,5 +206,13 @@ class Categoria
     public int Id { get; set; }
     public string Nome { get; set; }
     public bool Status { get; set; }
+
+}
+
+class ProdutoDto
+{
+  
+    public string Nome { get; set; }
+    public decimal Valor { get; set; }
 
 }
